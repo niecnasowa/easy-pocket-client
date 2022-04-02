7739c67fb10d117e69e9fd48f026f38ba043dc8a
Damian Sowa <niecnasowa@gmail.com>
2022-04-02 14:05:38 +0200

    init repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // proxy
+  async rewrites() {
+    return [
+      {
+        source: '/proxyApi/:path*',
+        destination: 'https://getpocket.com/v3/:path*' // Proxy to Backend
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
